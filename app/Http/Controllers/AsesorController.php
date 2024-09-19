3862d0b4ef1f0b53af0e4a22af36d7eaa02368ec
Jorge Omner Arias Olea~ <omnerarias001@gmail.com>
2024-09-18 20:22:24 -0600

    creando tablas asesores y empresas

---
+++ app/Http/Controllers/AsesorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Asesor;
+use App\Http\Requests\StoreAsesorRequest;
+use App\Http\Requests\UpdateAsesorRequest;
+
+class AsesorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAsesorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Asesor $asesor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Asesor $asesor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAsesorRequest $request, Asesor $asesor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Asesor $asesor)
+    {
+        //
+    }
+}
