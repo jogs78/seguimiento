034aa06215bedd80b293a728451c70f346b3f71f
Jorge Omner Arias Olea~ <omnerarias001@gmail.com>
2025-06-18 12:13:43 -0600

    home con logo

--- resources/views/acceso/adentro.blade.php
+++ resources/views/acceso/adentro.blade.php
@@ -1,7 +1,14 @@
 @extends('plantillas.app')
+<style>
+    .centro{display: flex; justify-content: center;}
+</style>
 @section('encabezado')
     <a href="{{route('Cambiar_Contraseña')}}">Cambiar la Contraseña</a>
 @endsection
 @section('contenido')
-    <h1>Bienvenido que bueno que entraste!! </h1>
+    <h1 class="centro">Sistema de seguimiento</h1> <br><br>
+   <div style="text-align: center;">
+        <img src="{{ asset('storage/img/logo.png') }}" alt="Logo"
+         style="width: 25%; height: auto;">
+    </div>
 @endsection
